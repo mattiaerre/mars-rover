207b8e8bd969f60d80b10453622adc885e2b9b2b
mattia.richetto <mattia.richetto@gmail.com>
2014-01-18 15:20:10 +0000

    implementing IRover Commands

--- FAMR.CORE.TEST/Rover_Test.cs
+++ FAMR.CORE.TEST/Rover_Test.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FAMR.CORE.Domain;
 using FAMR.CORE.Models;
 using NUnit.Framework;
@@ -16,11 +13,22 @@ namespace FAMR.CORE.TEST
     [SetUp]
     public void Given_A_Rover()
     {
-      _rover = new Rover();
+      var position = new PositionModel { X = 0, Y = 0, Orientation = Orientation.N };
 
+      _rover = new Rover(position);
     }
 
     [Test]
+    public void Orientatio_Fom_N_To_E_When_Command_R()
+    {
+      _rover.Commands(new List<Command> { Command.R });
+
+      var position = _rover.GetPosition();
+
+      Assert.AreEqual(Orientation.E, position.Orientation);
+    }
+
+    [Test, Explicit]
     public void Start_00N_Command_F_End_01N()
     {
       // arrange
@@ -37,7 +45,7 @@ namespace FAMR.CORE.TEST
       Assert.AreEqual(Orientation.N, result.Orientation);
     }
 
-    [Test]
+    [Test, Explicit]
     public void Start_00E_Command_F_End_10E()
     {
       // arrange
--- FAMR.CORE/Domain/Rover.cs
+++ FAMR.CORE/Domain/Rover.cs
@@ -5,13 +5,27 @@ namespace FAMR.CORE.Domain
 {
   public class Rover : IRover
   {
+    private readonly PositionModel _position;
+
+    public Rover(PositionModel position)
+    {
+      _position = position;
+    }
+
     public void Commands(List<Command> commands)
     {
+      foreach (var command in commands)
+        UpdatePosition(command);
+    }
+
+    private void UpdatePosition(Command command)
+    {
+      _position.Orientation = Orientation.E;
     }
 
     public PositionModel GetPosition()
     {
-      return new PositionModel();
+      return _position;
     }
   }
 }
