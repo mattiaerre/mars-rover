1b99915645e9fea70fd7b8e11a8ce63e6fcfef8e
mattia.richetto <mattia.richetto@gmail.com>
2014-01-18 16:04:42 +0000

    wrapping

--- FAMR.CORE.TEST/Rover_Test.cs
+++ FAMR.CORE.TEST/Rover_Test.cs
@@ -14,8 +14,9 @@ namespace FAMR.CORE.TEST
     public void Given_A_Rover()
     {
       var position = new PositionModel { Coordinates = new CoordinatesModel { X = 0, Y = 0 }, Orientation = Orientation.N };
+      var maxCoordinates = new CoordinatesModel { X = 100, Y = 100 };
 
-      _rover = new Rover(position);
+      _rover = new Rover(position, maxCoordinates);
     }
 
     [Test]
@@ -145,7 +146,7 @@ namespace FAMR.CORE.TEST
     }
 
     [Test]
-    public void Start_00N_Command_F_End_01N()
+    public void Position_From_00N_To_01N_When_Command_F()
     {
       var commands = new List<Command> { Command.F };
 
@@ -159,7 +160,7 @@ namespace FAMR.CORE.TEST
     }
 
     [Test]
-    public void Start_00N_Command_RF_End_10E()
+    public void Position_From_00N_To_10E_When_Command_RF()
     {
       var commands = new List<Command> { Command.R, Command.F };
 
@@ -170,6 +171,48 @@ namespace FAMR.CORE.TEST
       Assert.AreEqual(1, result.Coordinates.X);
       Assert.AreEqual(0, result.Coordinates.Y);
       Assert.AreEqual(Orientation.E, result.Orientation);
+    }
+
+    [Test]
+    public void Position_From_00N_To_0100N_When_Command_B()
+    {
+      var commands = new List<Command> { Command.B };
+
+      _rover.Commands(commands);
+
+      var result = _rover.GetPosition();
+
+      Assert.AreEqual(0, result.Coordinates.X);
+      Assert.AreEqual(100, result.Coordinates.Y);
+      Assert.AreEqual(Orientation.N, result.Orientation);
+    }
+
+    [Test]
+    public void Position_From_00N_To_1000E_When_Command_RB()
+    {
+      var commands = new List<Command> { Command.R, Command.B };
+
+      _rover.Commands(commands);
+
+      var result = _rover.GetPosition();
+
+      Assert.AreEqual(100, result.Coordinates.X);
+      Assert.AreEqual(0, result.Coordinates.Y);
+      Assert.AreEqual(Orientation.E, result.Orientation);
+    }
+
+    [Test]
+    public void Position_From_00N_To_100100N_When_Command_RBLB()
+    {
+      var commands = new List<Command> { Command.R, Command.B, Command.L, Command.B };
+
+      _rover.Commands(commands);
+
+      var result = _rover.GetPosition();
+
+      Assert.AreEqual(100, result.Coordinates.X);
+      Assert.AreEqual(100, result.Coordinates.Y);
+      Assert.AreEqual(Orientation.N, result.Orientation);
     }
   }
 }
--- FAMR.CORE/Domain/Rover.cs
+++ FAMR.CORE/Domain/Rover.cs
@@ -6,10 +6,12 @@ namespace FAMR.CORE.Domain
   public class Rover : IRover
   {
     private readonly PositionModel _position;
+    private readonly CoordinatesModel _maxCoordinates;
 
-    public Rover(PositionModel position)
+    public Rover(PositionModel position, CoordinatesModel maxCoordinates)
     {
       _position = position;
+      _maxCoordinates = maxCoordinates;
     }
 
     public void Commands(List<Command> commands)
@@ -45,6 +47,11 @@ namespace FAMR.CORE.Domain
         _position.Coordinates.X = _position.Coordinates.X - 1;
       else if (_position.Orientation == Orientation.W && command == Command.B)
         _position.Coordinates.X = _position.Coordinates.X + 1;
+
+      if (_position.Coordinates.X == -1)
+        _position.Coordinates.X = _maxCoordinates.X;
+      if (_position.Coordinates.Y == -1)
+        _position.Coordinates.Y = _maxCoordinates.Y;
     }
 
     private void UpdateOrientation(Command command)
@@ -53,14 +60,17 @@ namespace FAMR.CORE.Domain
         _position.Orientation = Orientation.E;
       else if (_position.Orientation == Orientation.N && command == Command.L)
         _position.Orientation = Orientation.W;
+
       else if (_position.Orientation == Orientation.E && command == Command.R)
         _position.Orientation = Orientation.S;
       else if (_position.Orientation == Orientation.E && command == Command.L)
         _position.Orientation = Orientation.N;
+
       else if (_position.Orientation == Orientation.S && command == Command.R)
         _position.Orientation = Orientation.W;
       else if (_position.Orientation == Orientation.S && command == Command.L)
         _position.Orientation = Orientation.E;
+
       else if (_position.Orientation == Orientation.W && command == Command.R)
         _position.Orientation = Orientation.N;
       else if (_position.Orientation == Orientation.W && command == Command.L)
